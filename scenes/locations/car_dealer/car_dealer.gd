07a527693291b1a2c4f22eab407c4dbf15d3cd50
Kirito <51691757+KoB-Kirito@users.noreply.github.com>
2023-04-30 23:32:05 +0200

    Auto stash before merge of "main" and "origin/main"

--- scenes/locations/car_dealer/car_dealer.gd
+++ scenes/locations/car_dealer/car_dealer.gd
@@ -1,6 +1,25 @@
 extends Node2D
 
 
+@onready var fade_in: ColorRect = %FadeIn
+@onready var carson: Area2D = %Carson
+
+
 func _ready() -> void:
+	# fade in
+	fade_in.visible = true
+	var tween = create_tween()
+	tween.tween_property(fade_in, "self_modulate", Color(0, 0, 0, 0), 1.0)
+	tween.tween_callback(start)
+
+
+func start():
+	# auto start
+	Dialogic.signal_event.connect(on_dialog_event)
 	var dialog = Dialogic.start("l2_scene3")
 	add_child(dialog)
+
+
+func on_dialog_event(arg):
+	if arg == "car":
+		carson.disable()

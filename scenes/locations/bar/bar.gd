578869e6929c60b8db0d5509e08b10ac33977196
Kirito <51691757+KoB-Kirito@users.noreply.github.com>
2023-05-01 23:36:37 +0200

    bar und transisions

--- scenes/locations/bar/bar.gd
+++ scenes/locations/bar/bar.gd
@@ -1,16 +1,47 @@
 extends Node2D
 
+const sorting_game = preload("res://scenes/locations/bar/sorting_game.tscn")
+
+@onready var sorting_game_playable: Area2D = %sorting_game_playable
+@onready var boris_talk: Area2D = %boris_talk
+@onready var fade_in: ColorRect = %FadeIn
+@onready var go_overworld: Area2D = %GoOverworld
+
 
 func _ready():
+	if Globals.location_3_cleared:
+		# disable all areas
+		sorting_game_playable.disable()
+		boris_talk.disable()
+		
+	# fade in
+	fade_in.visible = true
+	var tween = create_tween()
+	tween.tween_property(fade_in, "self_modulate", Color(0, 0, 0, 0), 1.0)
+	tween.tween_callback(start)
+
+
+func start():
+	if Globals.location_3_cleared:
+		# enable overworld map
+		go_overworld.enable()
+		return
+	
 	var dialog = Dialogic.start("l3_scene4_initizalize")
 	add_child(dialog)
 	Dialogic.signal_event.connect(on_dialog_event)
 
+
 func on_dialog_event(arg):
 	match arg:
 		"sorting_game":
-			get_tree().change_scene_to_file("res://scenes/locations/bar/sorting_game.tscn")
+			get_tree().change_scene_to_packed(sorting_game)
 		
 		"goodbye":
-			Globals.niece_unlocked = true
-			get_tree().change_scene_to_file("res://scenes/overworld.tscn")
+			Globals.location_3_cleared = true
+			# disable all areas
+			sorting_game_playable.disable()
+			boris_talk.disable()
+			
+			# enable overworld map
+			go_overworld.enable()
--- script/globals.gd
+++ script/globals.gd
@@ -3,4 +3,5 @@ extends Node
 
 var location_1_cleared: bool = false
 var location_2_cleared: bool = false
+var location_3_cleared: bool = false
 var niece_unlocked: bool = false

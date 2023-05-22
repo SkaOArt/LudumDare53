600ff1cebaf4e4122989ac2a7408cac0e8f47676
Kirito <51691757+KoB-Kirito@users.noreply.github.com>
2023-05-22 13:06:02 +0200

    wip translation kinda working

--- script/settings.gd
+++ script/settings.gd
@@ -14,3 +14,12 @@ func _on_check_button_toggled(button_pressed):
 
 func _on_audio_slider_value_changed(value):
 	AudioServer.set_bus_volume_db(0, value)
+
+
+func _on_option_button_item_selected(index: int) -> void:
+	match index:
+		0:
+			TranslationServer.set_locale("en")
+		
+		1:
+			TranslationServer.set_locale("de")
